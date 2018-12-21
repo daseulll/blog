2fb51aa88f8775773f23e8f889fb8bc7d0e1651b
nldaseul <nldaseul@gmail.com>
2018-12-21 17:06:07 +0900

    Add detail.html

--- feed/urls.py
+++ feed/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^$', views.detail, name='detail'),
+    url(r'^(?P<article_id>[0-9]+)/$', views.detail, name='detail'),
     # url(r'^$', views.about, name='about'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-
+from .models import Article, Comment, Hashtag
 # Create your views here.
 
 def index(request):
-    ctx = {}
+    article_list = Article.objects.all()
+    hashtag_list = Hashtag.objects.all()
+    ctx = {
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list,
+    }
     return render(request, "index.html", ctx)
 
-def detail(request):
-    pass
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    hashtag_list = Hashtag.objects.all()
+
+    ctx = {
+        "article" : article,
+        "hashtag_list" : hashtag_list,
+    }
+    return render(request, "detail.html", ctx)
 
 def about(request):
     pass
